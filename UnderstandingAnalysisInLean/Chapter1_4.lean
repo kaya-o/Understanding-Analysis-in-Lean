1bb0e12450d6c3e4c9bee6dd2ea3d19d3510bb63
ppaetsch <P.Paetsch@campus.lmu.de>
2025-03-02 16:03:10 +0100

    update theorem 1.4.2, add def theorem 1.4.11

--- UnderstandingAnalysisInLean/Chapter1_4.lean
+++ UnderstandingAnalysisInLean/Chapter1_4.lean
@@ -1,5 +1,6 @@
 import Mathlib.Data.Finset.Basic
 import Mathlib.Tactic
+import UnderstandingAnalysisInLean.Chapter1_3
 
 /-
 Chapter 1.4
@@ -11,7 +12,7 @@ then sequence of intervals has nonemtpy intersection ∩ I(n) ≠ ∅-/
 def interval (a b : ℝ) : Set ℝ := {x:ℝ | a ≤ x ∧ x ≤ b}
 
 /-
--- Theorem 1.4.2
+Theorem 1.4.2-/
 def all_natural_numbers : (Set ℕ) := { x | true }
 theorem archimedean_property_1 : ¬ is_bounded_above_N all_natural_numbers := by
     by_contra hcontra
@@ -22,35 +23,25 @@ theorem archimedean_property_1 : ¬ is_bounded_above_N all_natural_numbers := by
     let contra := counter_example this
     linarith
 
-theorem archimedean_property_2 : ∀ (y : ℝ), ∃ n, 1 / n < y := by
-    intro y
-    by_cases 1 < y
-    case pos hpos =>
-        use 2 / y
-        norm_num
-        linarith
-    case neg hpos =>  sorry
--/
+theorem archimedean_property_2 : ∀ (y : ℝ)(hy:y>0), ∃ n, 1 / n < y := by
+    intro y hy
+    use 2 / y
+    norm_num
+    linarith
 
 /-Theorem 1.4.3-/
 theorem density_Q_in_R: ∀ (a b:ℝ), a < b → ∃ (r: ℚ), a<r ∧ r<b:=by
     intro a b h
     sorry
 
-
 /-Corollar 1.4.4
 Given any real numbers a < b, there exists a irrational number t satisfying a < t < b
 Idea: predefine irrational number as a number x ∈ ℝ \ ℚ -/
 
-
-/-Theorem 1.4.5-/
-theorem square_root_existence: ∃ (α : ℝ), α^2 = 2:=by
-sorry
-
 --Definition 1.4.6
-def one_to_one (f : A → B):= ∀ (a1 a2 : A), a1 ≠ a2 → f a1 ≠ f a2
+def one_to_one (f : X → Y):= ∀ (x1 x2 : X), x1 ≠ x2 → f x1 ≠ f x2
 
-def onto (f : A → B):= ∀ (b : B), ∃ (a : A), f a = b
+def onto (f : X → Y):= ∀ (y : Y), ∃ (x : X), f x = y
 
 --Definition 1.4.7
 @[simp]
@@ -64,8 +55,34 @@ def countable (A) := ℕ ~ A
 def uncountable (A) := ¬ (ℕ ~ A)
 
 --Theorem 1.4.11
-theorem Q_is_countable: countable ℚ:= by
-sorry
+def neg_embedding : ℚ ↪ ℚ := ⟨(λ x ↦ -x), neg_injective⟩
+def A₁ (n : ℕ) : Finset ℚ :=
+    match n with
+    | 0 => ∅
+    | 1 =>
+        {0}
+    | (n+1) =>
+        let A'' := (Finset.image (λ (p : Fin (n+1)) ↦ (p.val : ℚ) / (((n+1) - p.val) : ℚ)) Finset.univ).filter (λ x ↦ x ≠ 0)
+        A'' ∪ A''.map neg_embedding
+
+def Aₙ (n : ℕ) : Finset ℚ :=
+    match n with
+    | 0 => ∅
+    | 1 =>
+        {0}
+    | (n+1) =>
+        let A'' := (Finset.image (λ (p : Fin (n+1)) ↦ (p.val : ℚ) / (((n+1) - p.val) : ℚ)) Finset.univ).filter (λ x ↦ x ≠ 0)
+        let A' := A'' ∪ A''.map neg_embedding
+        A' \ (Finset.range n).biUnion A₁
+
+/-theorem Q_is_countable: countable ℚ:= by
+    rw [countable]
+    simp
+    let f : ℕ → ℕ → ℕ := λ p q ↦
+        let n := p + q
+        let sum₀ := (Finset.image (λ (n₀ : Fin (n-1)) ↦ A n₀) Finset.univ).sum (λ x ↦ x.card)
+        let A₀ := (A n).sort (fun a b ↦ a < b)
+-/
 
 theorem R_is_uncountable: uncountable ℝ := by
     intro h
