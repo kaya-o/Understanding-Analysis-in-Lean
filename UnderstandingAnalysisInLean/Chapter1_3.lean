aebb07b2ba4c243f374dcb778756c9146ef5e039
Jesstine Wangsawibawa <wangsawibawa@cip.ifi.lmu.de>
2025-03-02 18:46:51 +0100

    Minor changes

--- UnderstandingAnalysisInLean/Chapter1_3.lean
+++ UnderstandingAnalysisInLean/Chapter1_3.lean
@@ -4,6 +4,8 @@ import Mathlib.Tactic
 /-
 Chapter 1.3
 -/
+
+-- Definition of bounded above
 @[simp]
 def is_bounded_above (A : Set ℝ) := ∃ (b : ℝ), ∀ a ∈ A, a ≤ b
 @[simp]
@@ -11,9 +13,11 @@ def is_bounded_above_N (A : Set ℕ) := ∃ (b : ℕ), ∀ a ∈ A, a ≤ b
 @[simp]
 def is_bounded_above_Q (A : Set ℚ) := ∃ (b : ℚ), ∀ a ∈ A, a ≤ b
 
+-- Definition of bounded below
 @[simp]
 def is_bounded_below (A : Set ℝ) := ∃ (b : ℝ), ∀ a ∈ A, a ≥ b
 
+-- Definition of upper bound
 @[simp]
 def is_an_upper_bound_of (s : ℝ) (A : Set ℝ) := ∀ a ∈ A, a ≤ s
 infixl:50 " is_an_upper_bound_of " => is_an_upper_bound_of
@@ -24,6 +28,7 @@ infixl:50 " is_an_upper_bound_of_N " => is_an_upper_bound_of_N
 def is_an_upper_bound_of_Q (s : ℚ) (A : Set ℚ) := ∀ a ∈ A, a ≤ s
 infixl:50 " is_an_upper_bound_of_Q " => is_an_upper_bound_of_Q
 
+-- Definition of lower bound
 @[simp]
 def is_a_lower_bound_of (s : ℝ) (A : Set ℝ) := ∀ a ∈ A, a ≥ s
 infixl:50 " is_a_lower_bound_of " => is_a_lower_bound_of
@@ -40,6 +45,7 @@ lemma if_is_bounded_above_then_has_upper_bound_N (A : Set ℕ) (h : is_bounded_a
     use h
     exact hb
 
+-- Supremum and infimum
 @[simp]
 def is_the_supremum_of (s : ℝ) (A : Set ℝ) := s is_an_upper_bound_of A ∧ ∀ x ∈ {x | x is_an_upper_bound_of A}, s ≤ x
 infixl:50 " is_the_supremum_of " => is_the_supremum_of
@@ -53,11 +59,18 @@ def is_max_of (s : ℝ) (A : Set ℝ):= ∃ s ∈ A, ∀ a ∈ A, s ≥ a
 infixl:50 " is_max_of " => is_max_of
 def is_min_of (s : ℝ) (A : Set ℝ) := ∃ s ∈ A, ∀ a ∈ A, s ≤ a
 
+/-
+Axiom of completeness
+Every nonempty set of real numbers that is bounded
+above has a least upper bound.
+-/
 axiom completeness : ∀ (A : Set ℝ) , A.Nonempty ∧ is_bounded_above A → ∃ s : ℝ , s is_the_supremum_of A
 
 
 /-
 Example 1.3.3
+Let A = { 1/n : n ∈ N} = {1, 1/2, 1/3, ...}
+supA = 1
 -/
 
 def A : Set ℝ := {x | ∃ n : ℕ, n > 0 ∧ x = 1 / n}
@@ -80,12 +93,18 @@ example : 1 is_the_supremum_of A := by
 
 /-
 Example 1.3.5
+
+(0, 2) = {x ∈ R : 0 < x < 2}
+[0, 2] = {x ∈ R : 0 ≤ x ≤ 2}
+Both sets are bounded above and have the same supremum.
+(0, 2) doesn't have a maximum while [0, 2] does.
+When a maximum exists then it is the supremum
 -/
 
 def openi : Set ℝ := {x | 0 < x ∧ x < 2}
 def closedi : Set ℝ := {x | 0 ≤ x ∧ x ≤ 2}
 
-lemma open_closed_b_above : is_bounded_above openi ∧ is_bounded_above closedi := by
+lemma open_closed_bounded_above : is_bounded_above openi ∧ is_bounded_above closedi := by
   constructor
   · simp
     use 2
@@ -98,7 +117,7 @@ lemma open_closed_b_above : is_bounded_above openi ∧ is_bounded_above closedi 
     simp [closedi] at ha
     linarith
 
-lemma open_closed_b_below : is_bounded_below openi ∧ is_bounded_below closedi := by
+lemma open_closed_bounded_below : is_bounded_below openi ∧ is_bounded_below closedi := by
   constructor
   · simp
     use 0
@@ -111,7 +130,7 @@ lemma open_closed_b_below : is_bounded_below openi ∧ is_bounded_below closedi 
     simp [closedi] at ha
     linarith
 
-lemma sup_open_closed_2 : 2 is_the_supremum_of openi ∧ 2 is_the_supremum_of closedi := by
+lemma sup_open_closed_is_2 : 2 is_the_supremum_of openi ∧ 2 is_the_supremum_of closedi := by
   constructor
   · constructor
     · simp[openi]
@@ -119,7 +138,8 @@ lemma sup_open_closed_2 : 2 is_the_supremum_of openi ∧ 2 is_the_supremum_of cl
       linarith
     intro x
     simp[openi]
-    intro h
+    contrapose!
+    intro h1
     sorry
   constructor
   · simp [closedi]
@@ -127,7 +147,7 @@ lemma sup_open_closed_2 : 2 is_the_supremum_of openi ∧ 2 is_the_supremum_of cl
   have h1 : 2 ∈ closedi := by simp [closedi]
   exact hb 2 h1
 
-lemma inf_open_closed_0 : 0 is_the_infimum_of openi ∧ 0 is_the_infimum_of closedi := by
+lemma inf_open_closed_is_0 : 0 is_the_infimum_of openi ∧ 0 is_the_infimum_of closedi := by
   constructor
   · constructor
     · simp[openi]
@@ -135,6 +155,7 @@ lemma inf_open_closed_0 : 0 is_the_infimum_of openi ∧ 0 is_the_infimum_of clos
       linarith
     intro x
     simp[openi]
+    contrapose!
     intro h
     sorry
   constructor
@@ -170,10 +191,12 @@ lemma max_is_supremum :∃ s, s is_max_of closedi ∧ s is_the_supremum_of close
   · simp[closedi]
     use 2
     norm_num
-  apply sup_open_closed_2.2
+  apply sup_open_closed_is_2.2
 
 /-
 Example 1.3.6
+
+S = {r ∈ Q : r^2 < 2}
 -/
 
 def S : Set ℚ := {r | r^2 < 2}
@@ -193,9 +216,12 @@ example : is_bounded_above_Q S := by
 
 /-
 Lemma 1.3.7
+
+Assume s ∈ R is an upper bound for a set A ⊆ R. Then, s = supA if and only if, for every choice of ε > 0, there exists an element a ∈ A
+satisfying s − ε < a
 -/
 
-lemma sup (A : Set ℝ ) (s : ℝ) (h1 : s is_an_upper_bound_of A): s is_the_supremum_of A ↔ ∀ ε : ℝ, ε > 0 → ∃ a ∈ A, s - ε < a := by
+lemma supremum (A : Set ℝ ) (s : ℝ) (h1 : s is_an_upper_bound_of A): s is_the_supremum_of A ↔ ∀ ε : ℝ, ε > 0 → ∃ a ∈ A, s - ε < a := by
   constructor
   · intro h ε hε
     simp at h h1
@@ -221,9 +247,12 @@ lemma sup (A : Set ℝ ) (s : ℝ) (h1 : s is_an_upper_bound_of A): s is_the_sup
 
 /-
 Exercise 1.3.2.b
+
+Assume i ∈ R is a lower bound for a set A ⊆ R. Then, i = inf A if and only if, for every choice of ε > 0, there exists an element a ∈ A satisfying
+i + ε > a.
 -/
 
-lemma inf (A : Set ℝ ) (i : ℝ) (h1 : i is_a_lower_bound_of A): i is_the_infimum_of A ↔ ∀ ε : ℝ, ε > 0 → ∃ a ∈ A, i + ε > a := by
+lemma infimum (A : Set ℝ ) (i : ℝ) (h1 : i is_a_lower_bound_of A): i is_the_infimum_of A ↔ ∀ ε : ℝ, ε > 0 → ∃ a ∈ A, i + ε > a := by
 constructor
 · intro h ε hε
   simp at h h1
@@ -249,24 +278,26 @@ linarith [hb a ha]
 
 /-
 Exercise 1.3.3.a
+
+Let A be bounded below, and define B = {b ∈ R : b is a lower bound for A}. Show that sup B = inf A.
 -/
 
 def B (A : Set ℝ) := {b | b is_a_lower_bound_of A}
 
 example (A : Set ℝ)(hA : is_bounded_below A):∀ a ∈ A,∀ b ∈ B A, ∃ s, s is_the_supremum_of (B A) ∧ s is_the_infimum_of A := by
   intro a ha b hb
-  obtain ⟨ab, ha1⟩ := hA
+  obtain ⟨ab, ha'⟩ := hA
 
   have h1 : (B A).Nonempty := by
     use ab
-    exact ha1
+    exact ha'
 
   have hBboundabove : ∀ a ∈ A, ∀ b ∈ B A, b ≤ a → is_bounded_above (B A) := by
     intro a ha b hb h
     simp
     use a
-    intro b1 hb1
-    exact hb1 a ha
+    intro b' hb'
+    exact hb' a ha
 
   have h2 : is_bounded_above (B A) := by
     apply hBboundabove a ha b hb
@@ -276,16 +307,18 @@ example (A : Set ℝ)(hA : is_bounded_below A):∀ a ∈ A,∀ b ∈ B A, ∃ s,
   use s
   constructor
   · exact hs
+  obtain ⟨hs1, hs2⟩ := hs
   constructor
   · intro a ha
-    obtain ⟨hs1, hs2⟩ := hs
     exact hs2 a fun a_1 a_2 ↦ a_2 a ha
   intro x hx
-  obtain ⟨hs1, hs2⟩ := hs
   exact hs1 x hx
+
 
 /-
 Exercise 1.3.7
+
+Prove that if a is an upper bound for A, and if a is also an element of A, then it must be that a = supA
 -/
 
 example (A : Set ℝ) (a : ℝ) : a is_an_upper_bound_of A ∧ a ∈ A → a is_the_supremum_of A := by
